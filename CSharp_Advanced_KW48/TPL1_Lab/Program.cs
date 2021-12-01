8e166ea8cddd5673aac712cbe57aea5b59176d4f
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2021-12-01 07:53:59 +0100

    LABS TPL

---
+++ CSharp_Advanced_KW48/TPL1_Lab/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TPL1_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* TPL Übung mit CancelationTokenSource
+             * In dieser Aufgabe soll die Methode ShowPrimeNumbers eine Möglichkeit bekommen, dass man 
+             * den Primzahlen Algorithmus von aussen beenden kann. 
+             * 
+             * Der Primzahlalgorithmus soll nach 20 Sekunden automatisch von der Main-Methode beendet werden 
+             */
+
+
+        }
+
+        public static void ShowPrimeNumbers()
+        {
+            long i = 0;
+
+            while (true)
+            {
+                i++;
+                if (chkprime(i))
+                {
+                    Console.WriteLine(i.ToString());
+                }
+            }
+        }
+
+        private static bool chkprime(long num)
+        {
+            for (long i = 2; i < num; i++)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+    }
+
+    
+}
---
+++ CSharp_Advanced_KW48/TPL2_Lab/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TPL2_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* In dieser Übung soll eine Ermittlung von Primzahlen mit TPL unterstützt werden
+             * Im ersten Task soll die Ermittlung der Primzahlen (GetPrimeNumbers) statt finden.
+             * 
+             * Im Folge-Task soll die Liste der Primzahlen in eine *.txt - Datei geschrieben werden: 
+             */
+        }
+
+        public static List<long> GetPrimeNumbers(long until)
+        {
+            List<long> retValues = new List<long>();
+            for (int i = 0; i < until; i++)
+            {
+                i++;
+                if (chkprime(i))
+                {
+                    retValues.Add(i);
+                }
+            }
+
+            return retValues;
+        }
+
+        private static bool chkprime(long num)
+        {
+            for (long i = 2; i < num; i++)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
